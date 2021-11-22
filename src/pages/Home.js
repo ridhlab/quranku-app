3effe2957b60db613155aed800cfd0ef67138165
Muhammad Ridwan <mohridwan1707@gmail.com>
2021-11-22 18:46:02 +0700

    add search bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,54 @@
 import { Container } from "@chakra-ui/layout";
+import { Box, Input, useColorModeValue } from "@chakra-ui/react";
 import axios from "axios";
 import React, { Component } from "react";
 import Surah from "../components/Surah";
 import { withRouter } from "react-router";
 
+const BgSearch = () => {
+  const bgColor = useColorModeValue("white", "gray.800");
+  return bgColor;
+};
 class Home extends Component {
-  state = {
-    surah: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      surah: [],
+      filteredSurah: [],
+      searchField: "",
+    };
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ searchField: e.target.value });
+    const filteredSurah = this.filterSurah(
+      this.state.surah,
+      this.state.searchField
+    );
+    this.setState({ filteredSurah: filteredSurah });
   };
 
   handleDetail = (id) => {
     this.props.history.push(`/surah/${id}`);
   };
 
+  filterSurah(surah, query) {
+    console.log(query);
+    if (!query) {
+      return surah;
+    }
+
+    return surah.filter((surah) => {
+      const surahName = surah.name.transliteration.id.toLowerCase();
+      return surahName.includes(query);
+    });
+  }
+
   async componentDidMount() {
     try {
       const res = await axios.get("https://api.quran.sutanlab.id/surah/");
-      this.setState({ surah: res.data.data });
+      this.setState({ surah: res.data.data, filteredSurah: res.data.data });
     } catch (err) {
       console.log(err);
     }
@@ -24,17 +56,33 @@ class Home extends Component {
 
   render() {
     return (
-      <Container maxW="container.md" py={4} px={{ base: 4, md: 0 }}>
-        {this.state.surah.map((surah) => {
-          return (
-            <Surah
-              key={surah.number}
-              surah={surah}
-              goDetail={this.handleDetail}
-            />
-          );
-        })}
-      </Container>
+      <>
+        <Box
+          py={4}
+          position="sticky"
+          top={20}
+          bgColor={BgSearch}
+          px={{ base: 4, md: 0 }}
+        >
+          <Input
+            placeholder="Cari surah..."
+            border="1px"
+            borderColor="brand.900"
+            onChange={this.handleChange}
+          />
+        </Box>
+        <Container maxW="container.md" py={2} px={{ base: 4, md: 0 }}>
+          {this.state.filteredSurah.map((surah) => {
+            return (
+              <Surah
+                key={surah.number}
+                surah={surah}
+                goDetail={this.handleDetail}
+              />
+            );
+          })}
+        </Container>
+      </>
     );
   }
 }
